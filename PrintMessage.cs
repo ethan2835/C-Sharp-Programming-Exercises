3e9f939d79ab828834695e175a96f8a79c31f8ae
Ethane Masserey <62582406+ethan2835@users.noreply.github.com>
2026-07-15 19:15:33 -0400

    Add files via upload

---
+++ PrintMessage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PrintMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //say something
+        Debug.Log("Im the best game object ever!");
+    }
+}
