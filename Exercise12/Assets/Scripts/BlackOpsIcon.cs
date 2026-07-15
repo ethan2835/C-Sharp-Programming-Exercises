aa59a96b4f7d41dddb0c787b55b8c14e02898807
Ethane Masserey <62582406+ethan2835@users.noreply.github.com>
2026-07-15 19:21:25 -0400

    Add files via upload

---
+++ Exercise12/Assets/Scripts/BlackOpsIcon.cs
@@ -0,0 +1,30 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackOpsIcon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the object moving
+        const float MinImpulseForce = 3f;
+        const float MaxImpulseForce = 5f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);
+    }
+    /// <summary>
+    /// Called on a collision
+    /// </summary>
+    /// <param name="col">collisioninfo</param>
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Ouch");
+
+    }
+}
