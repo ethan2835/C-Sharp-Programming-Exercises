9cba613812d32afb465b841ff289eff5cc8ce7d8
Ethane Masserey <62582406+ethan2835@users.noreply.github.com>
2026-07-15 19:23:41 -0400

    Add files via upload

---
+++ IfStatements/IfStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IfStatements
+{
+    /// <summary>
+    /// If Statements lecture code
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates various forms of if statements
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // prompt for and get user input
+            Console.Write("Pick up the shiny thing? (1 for yes, 2 for no): ");
+            int answer = int.Parse(Console.ReadLine());
+
+            // print appropriate message
+            if (answer == 1)
+            {
+                Console.WriteLine("You picked up the shiny thing!");
+            }
+            else if (answer == 2)
+            {
+                Console.WriteLine("You dont have the shiny thing!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input, please enter 1 or 2.");
+            }
+        }
+    }
+}
